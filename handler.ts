64e56f91a135a85f5a4038a4497efc0a5b1da926
neripark <neripark@gmail.com>
2022-01-31 20:14:25 +0900

    add lighthouse endpoint

--- handler.ts
+++ handler.ts
@@ -17,3 +17,20 @@ export const hello: Handler = (event: any) => {
     resolve(response)
   })
 }
+
+export const lighthouse: Handler = () => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'lighthouse done!'
+      },
+      null,
+      2
+    ),
+  };
+
+  return new Promise((resolve) => {
+    resolve(response)
+  })
+}
